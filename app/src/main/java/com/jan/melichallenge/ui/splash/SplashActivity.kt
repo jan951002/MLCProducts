47a18a2e1374fd8e77e059cc0835331b01149d69
jan95 <jatru95@gmail.com>
2021-07-06 14:48:09 -0500

    - The application icon is created. - The splash view is created. - The new activity that persists the screen changes is configured.

---
+++ app/src/main/java/com/jan/melichallenge/ui/splash/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.jan.melichallenge.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.jan.melichallenge.R
+import com.jan.melichallenge.base.BaseActivity
+import com.jan.melichallenge.databinding.ActivitySplashBinding
+import com.jan.melichallenge.ui.main.MainActivity
+import kotlinx.coroutines.delay
+
+class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
+
+    companion object {
+        private const val SPLASH_TIME_OUT = 2000L
+    }
+
+    override fun layoutRes(): Int = R.layout.activity_splash
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launchWhenCreated {
+            delay(SPLASH_TIME_OUT)
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
