49782b700e863b09379d1802b54d703886fa4d86
jan95 <jatru95@gmail.com>
2022-01-22 14:18:23 -0500

    The result and error model has been implemented in all modules. Removed the Retrofit library in the use case module as it's not required.

--- data/src/main/java/com/jan/melichallenge/data/product/ProductRemoteDataSource.kt
+++ data/src/main/java/com/jan/melichallenge/data/product/ProductRemoteDataSource.kt
@@ -1,7 +1,13 @@
 package com.jan.melichallenge.data.product
 
+import com.jan.melichallenge.domain.Error
 import com.jan.melichallenge.domain.Product
+import com.jan.melichallenge.domain.Result
 
 interface ProductRemoteDataSource {
-    suspend fun searchProducts(query: String, offset: Int): List<Product>
+
+    suspend fun searchProducts(
+        query: String,
+        offset: Int
+    ): Result<List<Product>, Error<String, Throwable>>
 }
